170def3db3b46ae03489050605e26dfb182bf3b5
Eraze86 <therese.rosvall@hotmail.se>
2021-11-24 10:15:43 +0100

    documentation

--- script.js
+++ script.js
@@ -3,14 +3,17 @@
 const body = document.body;
 const ul = document.createElement("ul");
 body.append(ul)
-//skapa en li
 
+//skapa en lista 
 
 let array=["måla","sy", "pyssla", "odla", "rita"];
+
+// skapa li och koppla i ul, koppla till listan. 
 
 for (let i = 0; i < array.length; i++){
     let li = document.createElement("li")
     li.innerHTML = array[i]
+    //i%2 betyder varannan. i%3 är var tredje med början på jämt (0)
 if(i%2===0){
     li.style.backgroundColor ="pink"
 }
